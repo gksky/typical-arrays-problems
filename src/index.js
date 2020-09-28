30d4b1ca29668c13d7a640eb502b14fce270b422
gksky <a.gutkovsky@gmail.com>
2020-09-28 18:40:14 +0100

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length > 0) {
+    var res = array[0];
+    for (i = 1; i < array.length; i++) {
+      if (array[i] < res) res = array[i];
+    }
+    return res;
+  } else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length > 0) {
+    var res = array[0];
+    for (i = 1; i < array.length; i++) {
+      if (array[i] > res) res = array[i];
+    }
+    return res;
+  } else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  var res = 0;
+  if (Array.isArray(array) && array.length > 0) {
+    for (i = 0; i < array.length; i++) {
+      res += array[i];
+    }
+    return res / array.length;
+  } else return 0;
 }
